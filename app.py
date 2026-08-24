778f75fcc2f18a63b413e1fbb0e08ed01b0dc68c
Hamza Senhaji <senhamza0304@gmail.com>
2026-08-24 16:53:42 +0100

    Prepare app for cloud deployment

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ print(generate_password_hash("admin123"))
 # =========================================================
 # DATABASE CONFIGURATION
 # =========================================================
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -31,11 +32,16 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"mysql+pymysql://"
     f"{os.getenv('DB_USER')}:"
     f"{os.getenv('DB_PASSWORD')}@"
-    f"{os.getenv('DB_HOST')}/"
+    f"{os.getenv('DB_HOST')}:"
+    f"{os.getenv('DB_PORT')}/"
     f"{os.getenv('DB_NAME')}"
 )
 
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+    "connect_args": {
+        "ssl": {}
+    }
+}
 
 db = SQLAlchemy(app)
 
